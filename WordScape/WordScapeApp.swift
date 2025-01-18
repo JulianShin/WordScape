a1f84d8e4043f65ced8b2a8d476e9d91170b5346
JulianShin <scwh9447@gmail.com>
2025-01-18 13:51:32 +0900

    Initial Commit

---
+++ WordScape/WordScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScapeApp.swift
+//  WordScape
+//
+//  Created by ChoongWhan Shin on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
